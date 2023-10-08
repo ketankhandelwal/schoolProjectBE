8619d9ab8308d0f50ef8777fddd08d3540cc09d6
Ketan Khandelwal <ketan.algoworks@swaniti.in>
2023-10-08 12:51:38 +0530

    ? check

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -14,23 +14,23 @@ export class AccountService {
         },
       },
       update: {
-        fees_per_student: data.fees_per_student,
-        total_students: data.total_student,
-        total_months_fees_collected: data.total_months_fees_collected,
+        fees_per_student: data?.fees_per_student,
+        total_students: data?.total_student,
+        total_months_fees_collected: data?.total_months_fees_collected,
       },
       create: {
-        fees_per_student: data.fees_per_student,
-        total_students: data.total_student,
-        class_id: data.class_id,
-        year: data.year,
-        total_months_fees_collected: data.total_months_fees_collected,
+        fees_per_student: data?.fees_per_student,
+        total_students: data?.total_student,
+        class_id: data?.class_id,
+        year: data?.year,
+        total_months_fees_collected: data?.total_months_fees_collected,
       },
     });
 
     const total_fees_amount =
-      result.total_students *
-      result.fees_per_student *
-      result.total_months_fees_collected;
+      result?.total_students *
+      result?.fees_per_student *
+      result?.total_months_fees_collected;
 
     const activeStudents = await this.prisma.student.findMany({
       where: {
@@ -44,6 +44,7 @@ export class AccountService {
 
     // Extract student IDs from the result
     const studentIds = activeStudents.map((student) => student.id);
+
 
     // Calculate the total amount collected for active students within a specific year and class
     const feesSum = await this.prisma.studentFees.groupBy({
@@ -72,6 +73,32 @@ export class AccountService {
   public async getSetFeesByClass(payload, class_id, year) {
     year = year || new Date(Date.now()).getFullYear();
 
+    const findStudentIds = await this.prisma.student.findMany({
+      where: {
+        class_id: Number(class_id),
+        status: 1,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const studentIds = findStudentIds.map((student) => student.id);
+
+    // Calculate the total amount collected for active students within a specific year and class
+    const feesSum = await this.prisma.studentFees.groupBy({
+      by: ["year"],
+      _sum: {
+        amount: true,
+      },
+      where: {
+        year: Number(year),
+        student_id: {
+          in: studentIds,
+        },
+      },
+    });
+
     const result = await this.prisma.setFees.findUnique({
       where: {
         class_year_unique_constraint: {
@@ -80,9 +107,15 @@ export class AccountService {
         },
       },
     });
-
     return {
-      res: result,
+      res: {
+        result,
+        remaingAmountForThisYear:
+          result?.fees_per_student *
+            result?.total_months_fees_collected *
+            result?.total_students -
+          feesSum[0]?._sum.amount,
+      },
     };
   }
 }
